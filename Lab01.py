8b5bb25fb91e7410bc6ed718fa8da43df60dbefa
Gitanuj993 <anujtawar42@gmail.com>
2026-03-26 12:45:14 +0530

    Labs are arranged and renamed

---
+++ Lab01.py
@@ -0,0 +1,17 @@
+"""
+Welcome AT
+
+aim : To learn newline,seperators , end arguements by which we can change the output according to us
+pre-requesite : basic things
+"""
+from traceback import print_tb
+
+print("Welcome AT")
+for i in range (2) :
+    print(" Welcmoe",end="1")
+
+print("hello","hii", "new", sep="*")
+
+
+
+
---
+++ lab3.py
@@ -0,0 +1,13 @@
+"""
+In this file we will discuss how to comments in python 
+
+We know that every programming language have two types of comment methods
+1. Single-line type  - only one line can't run by interpreter using '#' 
+2. Multiline type - Many lines can't be interpreted by interpreter using three double quotes like this comment has
+
+"""
+print("Welcome AT")
+# This is a single line comment
+""" 
+This is Multi-line comment
+"""
