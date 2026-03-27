98e3ce89565153eb60a508ae69c403c2a11b99f0
Gitanuj993 <anujtawar42@gmail.com>
2026-03-27 17:04:13 +0530

    lab 2 about strings added

--- lab2.py
+++ lab2.py
@@ -1,1 +1,20 @@
-data redundency
+"""
+Welcome AT
+AIM :	to  know about strings data type 
+"""
+"""
+Theory :  string is a collection of characters , used to store text .
+space is also included in  string
+"""
+import sys
+text = " Welcome AT "
+string = text
+print(text)
+
+print(f" Adress of text : { id(text)}")
+print(f" Adress of string: { id(string)}")
+print(f" Length of string as number of characters in the text : {len(text)}")
+print(f" Size of string internally is :	{ sys.getsizeof("") } ")
+print(f" Size of string internally is :	{ sys.getsizeof(text) } ")
+print(f" address of reference factor is :	{ sys.getrefcount(text)}")
+
