e6523a9acea71f2108c05de80a18215dea47d777
Gitanuj993 <anujtawar42@gmail.com>
2026-03-27 21:53:39 +0530

    lab1 in oopm added about classes and objects

---
+++ oopm/lab1.py
@@ -0,0 +1,18 @@
+"""
+Welcome AT
+lab1
+aim : Classes and objects in python
+"""
+#declearing a class : blueprint
+class Base :
+	pass
+	
+
+
+
+
+
+ 
+if __name__ == "__main__" :
+	#object of the class
+	obj = Base() 
