22f77f9e979dfa78c3170fd8cfe9be090cae983e
Gitanuj993 <anujtawar42@gmail.com>
2026-03-30 13:19:17 +0530

    pyjokes module exlored !

---
+++ try-fun/pyjokes.py
@@ -0,0 +1,13 @@
+"""
+Welcome AT
+module : pyjokes 
+aim :	to use pyjokes !
+procedure :	To use pyjokes on our system, first we install pyjokes module via  on terminal
+>>	pip install pyjokes
+after that we can use pyjokes module and get a joke using get_joke()
+"""
+import pyjokes
+
+print(pyjokes.get_joke())
+
+# you will see different jokes each time you run code !
