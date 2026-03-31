4a78b26f8a2d3d78ccae6e78275fb65737914aaf
Gitanuj993 <anujtawar42@gmail.com>
2026-03-31 12:22:05 +0530

    lab14_3 : string to float

---
+++ lab14_3.py
@@ -0,0 +1,34 @@
+"""
+Welcome AT
+aim : float type conversion
+"""
+# 1 . int to float
+print("-------------------------------------")
+print(f"int to float conversion ")
+num = 12
+num2float = float(num)
+print(f" num : {num} type casted to float is : {num2float} type {type(num2float)}")
+num = 0
+num2float = float(num)
+print(f" num : {num} type casted to float is : {num2float} type {type(num2float)}")
+num = -12
+num2float = float(num)
+print(f" num : {num} type casted to float is : {num2float} type {type(num2float)}")
+
+
+#2. string to float
+print("-------------------------------------")
+print(f" string to float conversion ")
+str1 = "123"
+str2float = float(str1)
+print(f" string 1 : {str1} type casted to float is : {str2float} type {type(str2float)}")
+
+
+
+#3. bool to float
+
+
+#4. None to float
+
+#5. float to float
+
