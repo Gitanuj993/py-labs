508ed4be2c495fc4469e71de1ffd669b4c607cf9
Gitanuj993 <anujtawar42@gmail.com>
2026-04-06 14:35:57 +0530

    for - else loop added

--- problems/is_prime_no/prime_no_v3.py
+++ problems/is_prime_no/prime_no_v3.py
@@ -14,11 +14,13 @@ for i in range(1,n+1) :
         composite.append(i)
     else :
         prime.append(i)
-
+else :
+    print("\t All things works fine \t")
 print(f" Total number of prime numbers upto {n} is : {len(prime)} [",end=" ")
 for i in prime :
     print(i, end = " ")
 print("]\n")
-print(f" Total number of prime numbers upto {n} is : {len(composite)} [",end = " ")
+print(f" Total number of composite numbers upto {n} is : {len(composite)} [",end = " ")
 for i in composite :
     print(i, end = " ")
+print("]\n")
