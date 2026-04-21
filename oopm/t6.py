4b73cf0836ec4756760154b78394b90dbc157bcf
Gitanuj993 <anujtawar42@gmail.com>
2026-04-21 14:42:57 +0530

    oopm lab test6

---
+++ oopm/t6.py
@@ -0,0 +1,41 @@
+#  constructor in python
+
+# Student Management system
+class students :
+    # scope of class students begin
+    # class variables
+
+    # __init__
+    def __init__(self,roll_no,name,std,marks=0):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+# database
+Properties of students
+# Students details
+    1name self, mother's, father's
+    roll_no
+    1class ; year
+    1data of birth
+    email id
+    phone number
+    whatsapp number
+    category
+
+
+
+"""
