5840ae4ad74b27facb8ba05038b553172f5e36dd
Gitanuj993 <anujtawar42@gmail.com>
2026-04-21 15:53:07 +0530

    Error case - Default constructor

--- oopm/t8.py
+++ oopm/t8.py
@@ -10,6 +10,6 @@ a,b,c = 0,0,0
 obj1 = Customer(a,b,c)
 
 # Error case try this
-# calling default constructor , default constructor is not declared yet
+# calling default constructor , default constructor  is not declared yet
 # obj2 = Customer()
 # TypeError: Customer.__init__() missing 3 required positional arguments: 'a', 'b', and 'c'
