26a12ee67952ef013651c1da63a624350180f637
Gitanuj993 <anujtawar42@gmail.com>
2026-04-21 15:49:40 +0530

    constructor overloading is nto supported in python confirmed

--- oopm/t8.py
+++ oopm/t8.py
@@ -11,5 +11,5 @@ obj1 = Customer(a,b,c)
 
 # Error case try this
 # calling default constructor , default constructor is not declared yet
-obj2 = Customer()
+# obj2 = Customer()
 # TypeError: Customer.__init__() missing 3 required positional arguments: 'a', 'b', and 'c'
