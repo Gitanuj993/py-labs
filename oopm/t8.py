5c960ce77c69245667c4b889af00e0a72fc32126
Gitanuj993 <anujtawar42@gmail.com>
2026-04-21 14:44:14 +0530

    oopm lab test8

---
+++ oopm/t8.py
@@ -0,0 +1,15 @@
+# default constructor
+class Customer :
+
+    def __init__(self,a,b,c):
+        print(" Welcome to the class")
+
+
+# a,b,c = 0
+a,b,c = 0,0,0
+obj1 = Customer(a,b,c)
+
+# Error case try this
+# calling default constructor , default constructor is not declared yet
+obj2 = Customer()
+# TypeError: Customer.__init__() missing 3 required positional arguments: 'a', 'b', and 'c'
