1eaca35b2de19315a74c20b6b1c812a8e27a9973
Gitanuj993 <anujtawar42@gmail.com>
2026-04-27 12:30:37 +0530

    file handling fi1

--- lab28-file_handling/f1.py
+++ lab28-file_handling/f1.py
@@ -1,5 +1,11 @@
 # File handling , working with files
 
-f = open("test1.txt","w")
-f.write("Welcome AT")
+# open is a global function or built-in function which return a string object
+f = open("test1.txt","r",encoding = 'utf-8')
+# f.write("Welcome AT")
+data = f.read() # .read() is a global function which reads the data from the file and return a strings
 f.close()
+
+print(data)
+print(f" Type of file f : {type(f)}")
+#
