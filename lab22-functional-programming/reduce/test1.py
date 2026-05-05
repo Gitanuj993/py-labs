5973d90cfc77e39f09a15a27617c128f24f382c4
Gitanuj993 <anujtawar42@gmail.com>
2026-05-05 13:22:53 +0530

    reduce test.py deleted

--- lab22-functional-programming/reduce/test1.py
+++
@@ -1,9 +0,0 @@
-# new class variables names ....
-from functools import reduce 
-
-test = [ 1 ,2,3,4,5]
-print(test)
-result  = reduce ( lambda x,y : x + y , test)
-print(result)
-factorial = reduce ( lambda x,y : x*y , test)
-print(factorial)
